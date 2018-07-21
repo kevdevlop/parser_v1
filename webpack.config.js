25d47cdfdb10eec0a2e6121af214c19f778d1c45
Kevdevlop <ing.kevinlopezsistemas@gmail.com>
2018-07-21 15:52:22 -0500

    ...

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+		uglifyOptions: {
+			output: {
+				ascii_only: true
+			}
+		}
+	  })
+    ]
+  },
+  module: {
+	   rules: [
+	     { test: /\.g4/, loader: 'antlr4-webpack-loader' }
+	   ]
+  },
+  node:{
+  	net: "empty", fs: "empty"
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+  
+};
